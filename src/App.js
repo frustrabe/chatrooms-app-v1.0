e77549a62de296d3a8a6755ffbb903bca036962b
frustrabe <lukas.forner@gmail.com>
2022-04-23 15:15:51 +0200

    started with styling part of chat app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Welcome to the chat app. If you are logged in, you can go to /chatrooms, else go to and /login.
+      Welcome to the chat app. If you are logged in, you can go to /chatrooms, else go and /login.
     </div>
   );
 }
